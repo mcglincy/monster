07e67d602bcdc1158d410ab076deb8c9f87b661a
mcglincy <mattmcglincy@gmail.com>
2020-02-27 07:42:28 -0600

    Check __main__ and move script body into main() method.

--- utils/evennia_object_prototype_generator.py
+++ utils/evennia_object_prototype_generator.py
@@ -98,121 +98,124 @@ def escaped(s):
   return s.replace('"', '\\"')
 
 
-##########
+def main():
+  """Command-line script."""
+  with open(OBJECT_FILE) as f:
+    objects = json.load(f)
+
+  with open(DESC_FILE) as f:
+    descs = json.load(f)
+
+  print("""# evmonster generated objects
+  #
+  #""")
+
+  # divide objects into lists of weapons and armor
+  weapons = []
+  armor = []
+  for obj in objects:
+    if obj['kind'] == ObjectKind.EQUIP:
+      if (lookup_effect(obj, Effect.WEAPON_BASE_DAMAGE)
+        or lookup_effect(obj, Effect.WEAPON_RANDOM_DAMAGE)):
+        weapons.append(obj)
+      elif (lookup_effect(obj, Effect.BASE_ARMOR)
+        or lookup_effect(obj, Effect.DEFLECT_ARMOR)
+        or lookup_effect(obj, Effect.SPELL_ARMOR)):
+        armor.append(obj)
+  weapons.sort(key=lambda x: x['obj_name'].upper())
+  armor.sort(key=lambda x: x['obj_name'].upper())
+
+  print("""
+  #
+  # Weapon prototypes
+  #
+
+  WEAPON = {
+    'typeclass': 'typeclasses.objects.Weapon',
+    'key': 'weapon',
+    'attack_speed': 0,
+    'base_damage': 0,
+    'desc': 'A weapon.',
+    'equip_slot': 1,
+    'random_damage': 0,
+    'weight': 0,
+    'worth': 0
+  }
+  """)
+
+  for weapon in weapons:
+  #  classname = camelcase_class_name(weapon['obj_name'])
+  #  print(f'class {classname}(Weapon):')
+  #  print('  pass')
+    obj_name = weapon['obj_name']
+    base_damage = lookup_effect(weapon, Effect.WEAPON_BASE_DAMAGE) or 0
+    random_damage = lookup_effect(weapon, Effect.WEAPON_RANDOM_DAMAGE) or 0
+    attack_speed = lookup_effect(weapon, Effect.ATTACK_SPEED) or 0
+    print(f"{snake_case(obj_name)} = {{")
+    # TODO: add better quote escaping for key and desc
+    print(f"  'key': \"{weapon['obj_name']}\",")
+    print("  'prototype_parent': 'WEAPON',")
+    print(f"  'attack_speed': {attack_speed},")
+    print(f"  'base_damage': {base_damage},")
+    desc_idx = weapon['examine'] - 1
+    if desc_idx >= 0 and desc_idx < len(descs):
+      # TODO: special handling for 'default' descript 32000
+      desc = escaped(' '.join(descs[desc_idx]['lines']))
+      print(f"  'desc': \"{desc}\",")
+    print(f"  'equip_slot': {weapon['wear']},")
+    # TODO: handle line_desc? looks mostly dead
+    print(f"  'random_damage': {random_damage},")
+    print(f"  'weight': {weapon['weight']},")
+    print(f"  'worth': {weapon['worth']},")
+    print('}')
+    print()
 
 
-with open(OBJECT_FILE) as f:
-  objects = json.load(f)
+  print("""
+  #
+  # Armor prototypes
+  #
 
-with open(DESC_FILE) as f:
-  descs = json.load(f)
+  ARMOR = {
+    'typeclass': 'typeclasses.objects.Armor',
+    'key': 'armor',
+    'base_armor': 0,
+    'deflect_armor': 0,
+    'desc': 'An armor.',
+    'equip_slot': 4,
+    'spell_armor': 0,
+    'spell_deflect_armor': 0,
+    'weight': 0,
+    'worth': 0  
+  }
+  """)
 
-print("""# evmonster generated objects
-#
-#""")
-
-# divide objects into lists of weapons and armor
-weapons = []
-armor = []
-for obj in objects:
-  if obj['kind'] == ObjectKind.EQUIP:
-    if (lookup_effect(obj, Effect.WEAPON_BASE_DAMAGE)
-      or lookup_effect(obj, Effect.WEAPON_RANDOM_DAMAGE)):
-      weapons.append(obj)
-    elif (lookup_effect(obj, Effect.BASE_ARMOR)
-      or lookup_effect(obj, Effect.DEFLECT_ARMOR)
-      or lookup_effect(obj, Effect.SPELL_ARMOR)):
-      armor.append(obj)
-weapons.sort(key=lambda x: x['obj_name'].upper())
-armor.sort(key=lambda x: x['obj_name'].upper())
-
-print("""
-#
-# Weapon prototypes
-#
-
-WEAPON = {
-  'typeclass': 'typeclasses.objects.Weapon',
-  'key': 'weapon',
-  'attack_speed': 0,
-  'base_damage': 0,
-  'desc': 'A weapon.',
-  'equip_slot': 1,
-  'random_damage': 0,
-  'weight': 0,
-  'worth': 0
-}
-""")
-
-for weapon in weapons:
-#  classname = camelcase_class_name(weapon['obj_name'])
-#  print(f'class {classname}(Weapon):')
-#  print('  pass')
-  obj_name = weapon['obj_name']
-  base_damage = lookup_effect(weapon, Effect.WEAPON_BASE_DAMAGE) or 0
-  random_damage = lookup_effect(weapon, Effect.WEAPON_RANDOM_DAMAGE) or 0
-  attack_speed = lookup_effect(weapon, Effect.ATTACK_SPEED) or 0
-  print(f"{snake_case(obj_name)} = {{")
-  # TODO: add better quote escaping for key and desc
-  print(f"  'key': \"{weapon['obj_name']}\",")
-  print("  'prototype_parent': 'WEAPON',")
-  print(f"  'attack_speed': {attack_speed},")
-  print(f"  'base_damage': {base_damage},")
-  desc_idx = weapon['examine'] - 1
-  if desc_idx >= 0 and desc_idx < len(descs):
-    # TODO: special handling for 'default' descript 32000
-    desc = escaped(' '.join(descs[desc_idx]['lines']))
-    print(f"  'desc': \"{desc}\",")
-  print(f"  'equip_slot': {weapon['wear']},")
-  # TODO: handle line_desc? looks mostly dead
-  print(f"  'random_damage': {random_damage},")
-  print(f"  'weight': {weapon['weight']},")
-  print(f"  'worth': {weapon['worth']},")
-  print('}')
-  print()
+  for armor in armor:
+    obj_name = armor['obj_name']
+    base_armor = lookup_effect(armor, Effect.BASE_ARMOR) or 0
+    deflect_armor = lookup_effect(armor, Effect.DEFLECT_ARMOR) or 0
+    spell_armor = lookup_effect(armor, Effect.SPELL_ARMOR) or 0
+    spell_deflect_armor = lookup_effect(armor, Effect.SPELL_DEFLECT_ARMOR) or 0
+    print(f"{snake_case(obj_name)} = {{")  
+    # TODO: add better quote escaping for key and desc
+    print(f"  'key': \"{armor['obj_name']}\",")
+    print("  'prototype_parent': 'ARMOR',")
+    print(f"  'base_armor': {base_armor},")  
+    print(f"  'deflect_armor': {deflect_armor},")  
+    desc_idx = armor['examine'] - 1
+    if desc_idx >= 0 and desc_idx < len(descs):
+      # TODO: special handling for 'default' descript 32000
+      desc = escaped(' '.join(descs[desc_idx]['lines']))
+      print(f"  'desc': \"{desc}\",")
+    print(f"  'equip_slot': {armor['wear']},")  
+    # TODO: handle line_desc? looks mostly dead
+    print(f"  'spell_armor': {spell_armor},")  
+    print(f"  'spell_deflect_armor': {spell_deflect_armor},")  
+    print(f"  'weight': {armor['weight']},")  
+    print(f"  'worth': {weapon['worth']},")  
+    print('}')
+    print()
 
 
-print("""
-#
-# Armor prototypes
-#
-
-ARMOR = {
-  'typeclass': 'typeclasses.objects.Armor',
-  'key': 'armor',
-  'base_armor': 0,
-  'deflect_armor': 0,
-  'desc': 'An armor.',
-  'equip_slot': 4,
-  'spell_armor': 0,
-  'spell_deflect_armor': 0,
-  'weight': 0,
-  'worth': 0  
-}
-""")
-
-for armor in armor:
-  obj_name = armor['obj_name']
-  base_armor = lookup_effect(armor, Effect.BASE_ARMOR) or 0
-  deflect_armor = lookup_effect(armor, Effect.DEFLECT_ARMOR) or 0
-  spell_armor = lookup_effect(armor, Effect.SPELL_ARMOR) or 0
-  spell_deflect_armor = lookup_effect(armor, Effect.SPELL_DEFLECT_ARMOR) or 0
-  print(f"{snake_case(obj_name)} = {{")  
-  # TODO: add better quote escaping for key and desc
-  print(f"  'key': \"{armor['obj_name']}\",")
-  print("  'prototype_parent': 'ARMOR',")
-  print(f"  'base_armor': {base_armor},")  
-  print(f"  'deflect_armor': {deflect_armor},")  
-  desc_idx = armor['examine'] - 1
-  if desc_idx >= 0 and desc_idx < len(descs):
-    # TODO: special handling for 'default' descript 32000
-    desc = escaped(' '.join(descs[desc_idx]['lines']))
-    print(f"  'desc': \"{desc}\",")
-  print(f"  'equip_slot': {armor['wear']},")  
-  # TODO: handle line_desc? looks mostly dead
-  print(f"  'spell_armor': {spell_armor},")  
-  print(f"  'spell_deflect_armor': {spell_deflect_armor},")  
-  print(f"  'weight': {armor['weight']},")  
-  print(f"  'worth': {weapon['worth']},")  
-  print('}')
-  print()
+if __name__ == "__main__":
+  main()
