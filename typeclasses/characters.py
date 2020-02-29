e5d7d02abcbab4b00e1fe1f4908314ae6dc83f17
mcglincy <mattmcglincy@gmail.com>
2020-02-28 19:21:16 -0600

    Add kill xp.

--- typeclasses/characters.py
+++ typeclasses/characters.py
@@ -13,6 +13,48 @@ from evennia.commands import cmdhandler
 from commands.combat import target_msg
 from typeclasses.health import health_msg
 
+
+def level_from_xp(xp):
+  return int(xp / 1000)
+
+
+# [GLOBAL]
+# PROCEDURE DieChangeExperience(Th : INTEGER; My : INTEGER; VAR Stat : StatType);
+# VAR
+#   Mylevel, R : INTEGER;
+# BEGIN
+#   Th := Th DIV 1000;  (* making it the level, not the exp *)
+#   Mylevel := My DIV 1000;
+#   IF (Mylevel = 0) OR (Mylevel < Th) THEN
+#   BEGIN
+#     IF (((TH/2) + 1.5 - Mylevel) > 0) THEN
+#       R := Round(1000*((Th/2) + 1.5 - Mylevel))
+#     ELSE R := Round(1000*(1/(Mylevel+(Mylevel-th))));
+#   END
+#   ELSE
+#   BEGIN
+#     IF (Mylevel >= Th) AND (Mylevel < 10) AND ((Mylevel - Th) < 3)THEN
+#       R := Round(1000*(1/(Mylevel+(Mylevel-Th))))
+#    ELSE
+#      R := 0;
+#    END;
+#    ChangeExp(R, Stat.Log, Stat.Experience);
+# END;
+def kill_xp(killer_xp, victim_xp):
+  killer_level = level_from_xp(killer_xp)
+  victim_level = level_from_xp(victim_xp)
+  levels_above = killer_level - victim_level
+  amt = 0
+  if killer_level == 0 or killer_level < victim_level:
+    multiplier = (victim_level / 2) + 1.5 - killer_level
+    if multiplier < 0:
+      multiplier = 1 / (killer_level + levels_above)
+    amt = int(round(1000 * multiplier))
+  elif killer_level >= victim_level and killer_level < 10 and levels_above < 3:
+    amt = int(round(1000 * (1 / (killer_level + levels_above))))
+  return amt
+
+
 class Character(DefaultCharacter):
   """
   The Character defaults to reimplementing some of base Object's hook methods with the
@@ -36,15 +78,27 @@ class Character(DefaultCharacter):
   def at_object_creation(self):
     """Called at initial creation."""
     super().at_object_creation()
-    # TODO: figure health etc from level and class
-    self.db.max_health = 1000
-    self.db.health = 1000
-    self.db.brief_descriptions = False
-    # TODO: support various equipment slots
-    self.db.equipped_weapon = None
-    self.db.equipped_armor = None
-    self.db.gold_in_bank = 0
+    self.set_field_defaults()
 
+  def set_field_defaults(self):
+    """Set various field defaults in an idempotent way."""
+    # TODO: figure health etc from level and class
+    if self.db.max_health is None:
+      self.db.max_health = 1000
+    if self.db.health is None:
+      self.db.health = 1000
+    if self.db.brief_descriptions is None:
+      self.db.brief_descriptions = False      
+    # TODO: support various equipment slots
+    # checking None to set None is pointless
+    if not self.db.equipped_weapon:
+      self.db.equipped_weapon = None
+    if not self.db.equipped_armor:
+      self.db.equipped_armor = None
+    if self.db.gold_in_bank is None:
+      self.db.gold_in_bank = 0
+    if self.db.xp is None:
+      self.db.xp = 1000
 
   def execute_cmd(self, raw_string, session=None, **kwargs):
     """Support execute_cmd(), like account and object."""
@@ -79,15 +133,15 @@ class Character(DefaultCharacter):
         self.msg("The attack is partially blocked by your armor.")
         attacker.msg(f"Your weapon is partially blocked by {self.key}'s armor.")
         damage = int(damage * ((100 - armor.db.base_armor) / 100))
-    self.at_damage(damage)
+    self.at_damage(damage, causer=attacker)
 
-  def at_damage(self, damage):
+  def at_damage(self, damage, causer=None):
     self.db.health = max(self.db.health - damage, 0)
     self.msg(f"You take {damage} damage.")
     self.msg(health_msg("You", self.db.health))
     self.location.msg_contents(health_msg(self.key, self.db.health), exclude=[self])
     if self.db.health <= 0:
-      self.die()
+      self.die(causer)
 
   def at_heal(self, amount):
     self.db.health = min(self.db.health + amount, self.db.max_health)
@@ -101,16 +155,47 @@ class Character(DefaultCharacter):
       return gold[0].db.amount
     return 0
 
-  def die(self):
+  def at_kill(self, deceased):
+    self.msg(f"You killed {deceased.key}!")
+    xp = kill_xp(self.db.xp, deceased.db.xp)
+    self.at_gain_xp(xp)
+
+  def die(self, causer=None):
     # drop everything we're holding
     for obj in self.contents:
       if obj.db.worth:
         # only drop things with value
         # TODO: possible destroy chance?
         self.execute_cmd(f"drop {obj.key}")
+      else:
+        # nuke worthless objects
+        obj.delete()
 
-    # TODO: go to the actual void room
-    the_void = search_object("Limbo")[0]
+    the_void = search_object("Void")[0]
     if the_void:
       self.move_to(the_void)
+
+    if causer and hasattr(causer, "at_kill"):
+      causer.at_kill(self)
+
     self.db.health = 200
+    self.at_set_xp(int(self.db.xp / 2))
+
+  def level(self):
+    return level_from_xp(self.db.xp)
+
+  def classname(self):
+    # TODO: pull from character class
+    return "Peasant"
+
+  def at_gain_xp(self, xp):
+    new_xp = max(1000, self.db.xp + xp)
+    self.at_set_xp(new_xp)
+
+  def at_set_xp(self, new_xp):
+    old_level = self.level()
+    self.db.xp = new_xp
+    new_level = self.level()
+    if old_level != new_level:
+      self.msg(f"You are now level {new_level}.")
+
