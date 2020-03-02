2c8c361f405b0a240c7dbd959c07c1d67b06f61d
mcglincy <mattmcglincy@gmail.com>
2020-03-02 10:01:55 -0600

    Set/reset character class via record_id.

--- gamerules/character_classes.py
+++ gamerules/character_classes.py
@@ -2,22 +2,20 @@ from gamerules.xp import set_xp
 from userdefined.models import CharacterClass
 
 
-def reset_character_class(target, character_class_key):
-  set_character_class(target, character_class_key)
+def reset_character_class(target, record_id):
+  set_character_class(target, record_id)
   set_xp(target, 0)
 
 
-def set_character_class(target, character_class_key):
+def set_character_class(target, record_id):
   # make sure this is a valid class
   try:
-    char_class = CharacterClass.objects.get(db_key=self.db.character_class_key)
+    char_class = CharacterClass.objects.get(db_record_id=record_id)
+    target.db.character_class_key = char_class.db_key
+    # force re-cache
+    target.character_class()
+    target.msg(f"You are now a {char_class.db_key}.")
   except:
     # Show error somewhere?
-    target.msg(f"There is no character class {character_class_key}.")
+    target.msg(f"Could not set character class {record_id}.")
     return
-  target.db.character_class_name = exit_effect_value
-  # force re-cache
-  target.character_class()
-  target.msg(f"You are now a {character_class_key}.")
-
-
