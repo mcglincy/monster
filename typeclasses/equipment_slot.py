45f1d83d95eb751b66af279da1d53c89c0d5a76c
mcglincy <mattmcglincy@gmail.com>
2020-03-05 23:20:56 -0600

    Equipment slot enum.

---
+++ typeclasses/equipment_slot.py
@@ -0,0 +1,23 @@
+from enum import IntEnum
+
+class EquipmentSlot(IntEnum):  
+  NOT_EQUIPPABLE = 0
+  SWORD_HAND = 1
+  SHIELD_HAND = 2
+  TWO_HAND = 3
+  BODY = 4
+  HEAD = 5
+  EYES = 6
+  NECK = 7
+  BACK = 8
+  UPPER_TORSO = 9
+  ARMS = 10
+  WRIST = 11
+  RING = 12
+  HAND = 13
+  WAIST = 14
+  LEGS = 15
+  FEET = 16
+  BACKPACK = 17
+  POUCH = 18
+  QUIVER = 19
