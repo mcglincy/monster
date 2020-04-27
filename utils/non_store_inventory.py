459af06e937b7045a2048308d7974b5c8d767922
mcglincy <mattmcglincy@gmail.com>
2020-04-27 09:31:45 -0500

    Add non_store_inventory.

---
+++ utils/non_store_inventory.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+from enum import IntEnum
+import json
+import sys
+sys.path.insert(0, '..')
+
+from gamerules.exit_effect_kind import ExitEffectKind
+from gamerules.exit_kind import ExitKind
+from utils.generator_utils import *
+
+with open('./json/objects.json') as f:
+  OBJECTS = json.load(f)
+with open('./json/roomdesc.json') as f:
+  ROOMDESCS = json.load(f)
+with open('./json/rooms.json') as f:
+  ROOMS = json.load(f)
+
+
+# TODO: move this somewhere (utils?)
+def check_bit(num, offset):
+  mask = 1 << offset
+  return(num & mask)
+
+
+def main():
+  """Command-line script."""
+  room_inv = []
+  for roomdesc in ROOMDESCS:
+    if check_bit(roomdesc["spc_room"], 0):
+      # skip STORE rooms
+      continue    
+    rec = {}
+    rec["room"] = roomdesc["nice_name"]
+    room_id = roomdesc["id"]
+    room = find_object(ROOMS, room_id)
+    inventory = []
+    for packed_object_int, packed_hide_int in zip(room["objs"], room["obj_hides"]):
+      if packed_object_int:
+        object_id = packed_object_int % 1000
+        condition = int(packed_object_int / 1000)
+        hide = packed_hide_int % 1000
+        charges = int(packed_hide_int / 1000)
+        obj = find_object(OBJECTS, object_id)
+        inventory.append({
+          "object": obj["obj_name"],
+          "condition": condition,
+          "charges": charges,
+          "hide": hide,
+        })
+    if inventory:
+      # only include rooms with objects
+      rec["inventory"] = inventory
+      room_inv.append(rec)
+  print(json.dumps(room_inv, indent=2))
+
+if __name__ == "__main__":
+  main()
