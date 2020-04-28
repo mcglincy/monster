0e079f5a2ff42355da7a86fb94ca4fe86a3033a5
mcglincy <mattmcglincy@gmail.com>
2020-04-28 14:52:08 -0500

    Add utility to fill in spell descriptions for easy bulk editing.

---
+++ utils/make_spell_import_data.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+import sys
+sys.path.insert(0, '..')
+
+import json
+from gamerules.spell_effect_kind import SpellEffectKind
+from utils.generator_utils import lookup_description
+
+
+CLASSREC_FILE = "./json/classrec.json"
+DESC_FILE = './json/desc.json'
+LINES_FILE = './json/lines.json'
+SPELLS_FILE = "./json/spells.json"
+
+
+def fill_in(rec, key, descs, lines):
+  desc_id = rec[key]
+  if desc_id:
+    rec[key] = lookup_description(desc_id, descs, lines)
+  else:
+    rec[key] = ""
+
+
+def create_spells():
+  with open(DESC_FILE) as f:
+    descs = json.load(f)
+  with open(LINES_FILE) as f:
+    lines = json.load(f)
+  with open(SPELLS_FILE) as f:
+    spells = json.load(f)
+
+  for rec in spells:
+    fill_in(rec, "caster_desc", descs, lines)
+    fill_in(rec, "victim_desc", descs, lines)
+    fill_in(rec, "failure_desc", descs, lines)
+
+  print(json.dumps(spells, indent = 2, sort_keys=False))    
+
+
+if __name__ == "__main__":
+  create_spells()
